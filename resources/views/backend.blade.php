d5529ffc2b93c54ac12a0e7eff381b32a6c91fa3
Adnan ALazaat <33832766+MhdAdnanALazaat@users.noreply.github.com>
2019-11-19 09:23:54 +0100

    top links for frontend/backend added

--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -2,6 +2,16 @@
 
 @section('title','Willkommen')
 
+@section('top-links')
+    @auth
+        <a href="/backend">Patient</a>
+        <a href="/backend">kalender</a>
+        <a href="/backend">Doku</a>
+        <a href="/backend">Frontend</a>
+
+
+    @endauth
+@endsection
 @section('main')
    <br>
     Willkomen bei AIOT.
--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -2,6 +2,11 @@
 
 @section('title','Willkommen')
 
+@section('top-links')
+    @auth
+    <a href="/backend">Backend</a>
+    @endauth
+@endsection
 @section('main')
    <br>
     Willkomen bei AIOT.
